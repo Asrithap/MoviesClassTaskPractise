7c709bbc33a461e7edf53cd5272a4a0fa9f5760d
Asritha Pokuru <asrithapokuru777@gmail.com>
2022-01-19 12:55:20 +0530

    solved switch issue by adding extensions

--- src/App.js
+++ src/App.js
@@ -2,14 +2,53 @@ import './App.css';
 import React from "react";
 import { ColorList } from './ColorList';
 import { MoviesList } from './MoviesList';
+import {Link,Switch,Route } from "react-router-dom";
+
+
+
 
 function App() {
   return (
     <div className="App">
       {/* <Counter/> */}
-      <MoviesList/>
+     
       {/* <ColorList/> */}
-    </div>      
+      {/* <React_Router/> */}
+      <nav>
+      <ul>
+        <li>
+        <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/movies">Movies</Link>
+        </li>
+        <li>
+        <Link to="/color">Color Game</Link>
+        </li>
+        
+      </ul>
+      </nav>
+      <Switch>
+  <Route path="/movies">
+  <MoviesList/>
+  
+  </Route>
+  <Route path="/color">
+  <ColorList/>
+  </Route>
+  <Route path="/">
+    <Welcome/>
+  </Route>
+</Switch>
+      </div>      
+  )
+}
+       {/* <MoviesList/> */}
+      
+function Welcome()
+{
+  return(
+    <h2>Home, Welcome All!!!</h2>
   )
 }
 
